e97f01f80d1e43fafe7172308639b38b3dde3052
shubhi0204 <shubhishukla0204@gmail.com>
2022-11-21 23:17:26 +0530

    push data

---
+++ lost-and-found/src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import Home from './Home';
+import  Footer from './Footer';
+import Nav from './Nav';
+
+function App() {
+ <Nav />
+ <Home />
+ <Footer />
+
+  
+
+}
+
+export default App;
